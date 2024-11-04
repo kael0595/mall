77d106fd0d03a30f0ee3ee3c10a1a85237c8d24b
kael0595 <changgyu549@gmail.com>
2024-11-04 18:18:51 +0900

    MemberController & MemberService & memberRepository write

---
+++ src/main/java/com/example/demo/member/controller/MemberController.java
@@ -0,0 +1,14 @@
+package com.example.demo.member.controller;
+
+import com.example.demo.member.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/member")
+public class MemberController {
+
+    private final MemberService memberService;
+}
